245bab3cab98d47f4967a5c6e4beaecf26859e09
EbOwusu20 <owusuemmanuelbanahene584@gmail.com>
2026-05-09 22:06:23 +0000

    Changes to my code

--- momo.py
+++ momo.py
@@ -1,12 +1,55 @@
-secret_pin = "2345"
-balance = 1000
-
-pin = input("Enter your pin: ")
-if pin == secret_pin:
-    print("Welcome to your account!")
-    print("Your balance is: ", balance) 
-
-else:    print("Incorrect pin. Please try again.")
 
 
+code = input("Enter code: ")
 
+if code != "*170#":
+    print("Wrong Code")
+
+else:
+    print("\n1) Transfer Money")
+    print("2) MomoPay")
+    print("3) Airtime")
+    print("4) Allow Cashout")
+    print("5) Financial")
+    print("#) Next")
+
+    option = input("Enter option: ")
+
+    if option == "#":
+
+        print("\n6) My Wallet")
+        print("7) Just4U")
+        print("8) MoMo App")
+
+        wallet = input("Enter option: ")
+
+        if wallet == "6":
+
+            print("\n1) Check Balance")
+            print("2) Allow Cash Out")
+            print("3) My Approvals")
+            print("4) Report Fraud")
+            print("5) Statements")
+
+            balance_option = input("Enter option: ")
+
+            if balance_option == "1":
+
+                pin = input("\nEnter MoMo PIN: ")
+
+                if pin == "2345":
+
+                    print("\nCurrent Balance: GHS 10000.00")
+                    print("Available Balance: GHS 10000.00")
+
+                else:
+                    print("Wrong PIN")
+
+            else:
+                print("Invalid Option")
+
+        else:
+            print("Invalid Option")
+
+    else:
+        print("Invalid Option")
